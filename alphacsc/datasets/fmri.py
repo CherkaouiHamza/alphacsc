4ce6bcd7b77933d71c6cb7cbffbf9fbffc6c01f8
Hamza CHERKAOUI <hamza.cherkao@gmail.com>
2019-03-01 12:32:22 +0100

    ENH simplify toy fmri dataset

--- alphacsc/datasets/fmri.py
+++ alphacsc/datasets/fmri.py
@@ -1,10 +1,6 @@
 # Authors: Hamza Cherkaoui <hamza.cherkaoui@inria.fr>
 
-import os
-import tempfile
-
 import numpy as np
-from scipy import io, sparse
 
 from ..utils import check_random_state
 from ..utils.dictionary import get_uv
@@ -20,46 +16,89 @@ def _gen_checkerboard(n_r=5, n_c=5, s=10):
     return np.kron([[1.0, 0.0] * n_c, [0.0, 1.0] * n_c] * n_r, np.ones((s, s)))
 
 
-def _blocks_signal(T=300, n=10):
+def _gen_single_case_checkerboard(p, n=5):
+    """ Return a matrice of 0.0 with only one block one 1.0. """
+    d = int(p/n)
+    b = (slice(d, 2*d), slice(d*(n-2), d*(n-1)))
+    u = np.zeros((p, p))
+    u[b] = 1.0
+    return u
+
+
+def _blocks_signal(T=300, n=10, rng=np.random):
     """ return a 1d signal of n blocks of length T
     """
     d = int(T / n)
-    return np.array(([1.0] * d + [0.0] * d) * (n // 2) + [0.0] * (n % 2))
+    s = []
+    for _ in range(n // 2):
+        a = 1.0 + np.abs(0.5 * rng.randn())
+        s += [a] * d + [0.0] * d
+    s += [0.0] * (n % 2)
+    return np.array(s)
 
 
-def load_synth_fmri_data(_P=10000, _T=300, L=30, sigma=0.1, random_seed=None):
+def _2_blocks_signal(T=300, n=10, rng=np.random):
+    """ return a 1d signal of n blocks of length T
+    """
+    d = int(T/n)
+    b_11 = slice(d, 2*d)
+    b_12 = slice(d*(n-4), d*(n-3))
+    b_21 = slice(3*d, 4*d)
+    b_22 = slice(d*(n-2), d*(n-1))
+    Lz_0 = np.zeros(T)
+    Lz_1 = np.zeros(T)
+    Lz_0[b_11] = 1.0 + np.abs(0.5 * rng.randn())
+    Lz_0[b_12] = 1.0 + np.abs(0.5 * rng.randn())
+    Lz_1[b_21] = 1.0 + np.abs(0.5 * rng.randn())
+    Lz_1[b_22] = 1.0 + np.abs(0.5 * rng.randn())
+    return Lz_0, Lz_1
+
+
+def add_gaussian_noise(signal, snr, random_state=None):
+    """ Add a Gaussian noise as targeted by the given SNR value.
+    """
+    rng = check_random_state(random_state)
+    noise = rng.randn(*signal.shape)
+    true_snr_num = np.linalg.norm(signal)
+    true_snr_deno = np.linalg.norm(noise)
+    true_snr = true_snr_num / (true_snr_deno + np.finfo(np.float).eps)
+    std_dev = (1.0 / np.sqrt(10**(snr/10.0))) * true_snr
+    noise = std_dev * noise
+    noisy_signal = signal + noise
+    return noisy_signal, noise, std_dev
+
+
+def load_synth_fmri_data(_P=100, _T=100, L=20, nb_square=2, snr=1.0,
+                         eta=10.0, random_seed=None):
     """ Generate BOLD data and its corresponding spatial map.
     """
+    # eta fix to 10.0 to match the actuel model in alphacsc
+
     rng = check_random_state(random_seed)
     N, K = 1, 2
 
     # z
-    Lz_0 = _blocks_signal(_T, n=10)
-    Lz_1 = np.abs(Lz_0 - 1.0)
+    Lz_0, Lz_1 = _2_blocks_signal(T=_T, rng=rng)
+    Lz_0 -= np.mean(Lz_0)
+    Lz_1 -= np.mean(Lz_1)
     z_0 = np.append(np.diff(Lz_0), 0)
     z_1 = np.append(np.diff(Lz_1), 0)
     Lz = np.vstack([Lz_0, Lz_1])[None]
     z = np.vstack([z_0, z_1])[None]
     T = Lz_0.shape[0]
 
-    # add noise on z
-    noisy_Lz = Lz + sigma * rng.randn(*Lz.shape)
-
     # v
     v = spm_hrf(1.0, L)[:, None]
     v /= np.linalg.norm(v)
 
     # normalized each u_i
-    s = int(np.sqrt(_P) / 10.0)
-    assert s != 0, "gen_fmri_synth_data called with P too small"
-    n_r = n_c = int(np.sqrt(_P) / (2 * s))
-    p = 2 * n_r * s
-
-    u_0 = _gen_checkerboard(n_r, n_c, s)
+    p = int(np.sqrt(_P))
+    u_0 = _gen_single_case_checkerboard(p)
+    u_1 = u_0.T
     u_0 = u_0.flatten()[:, None]
-    u_1 = np.abs(u_0 - 1.0)
-    u_0 /= np.linalg.norm(u_0)
-    u_1 /= np.linalg.norm(u_1)
+    u_1 = u_1.flatten()[:, None]
+    u_0 *= (eta / np.sum(np.abs(u_0)))
+    u_1 *= (eta / np.sum(np.abs(u_1)))
     u_i = [u_0, u_1]
     P = u_0.size
 
@@ -69,6 +108,7 @@ def load_synth_fmri_data(_P=10000, _T=300, L=30, sigma=0.1, random_seed=None):
     D = np.stack([uv_0, uv_1])
     uv = get_uv(D)
 
+    # check dimension
     assert Lz.shape == (N, K, T)
     assert z.shape == (N, K, T)
     assert v.shape == (L, 1)
@@ -78,7 +118,7 @@ def load_synth_fmri_data(_P=10000, _T=300, L=30, sigma=0.1, random_seed=None):
     cst = [T, P, p, K, N, L]
 
     # X
-    noisy_X = construct_X_multi(noisy_Lz, D, n_channels=P)
     X = construct_X_multi(Lz, D, n_channels=P)
+    noisy_X, _, _ = add_gaussian_noise(X, snr, random_state=random_seed)
 
-    return noisy_X, X, uv, D, u_i, v, noisy_Lz, Lz, z, cst
+    return noisy_X, X, uv, D, u_i, v, Lz, z, cst
